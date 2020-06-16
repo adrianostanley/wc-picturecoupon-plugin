cd67e62fbb90903d64ec7348414511bb3a1bed66
Adriano Castro <adrianostanley@gmail.com>
2020-06-16 08:51:47 -0300

    Project creation and composer files

---
+++ wc-picturecoupon-plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Picture Coupon Plugin
+ * Description: Allows customers to upload multiple profile pictures and have better discount codes on products.
+ * Author: Adriano Castro
+ * Version: 0.1
+ * WC requires at least: 3.0.9
+ * WC tested up to: 4.1.0
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package   PictureCoupon
+ * @author    ACastro
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
